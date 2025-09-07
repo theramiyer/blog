cbed91d153e4bb344088e25b35ee3c179ab92c80
Ram Iyer <2492952+theramiyer@users.noreply.github.com>
2025-09-07 23:43:15 +0530

    Enable local deployment

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Build the site
+hugo --minify
+
+# Compress the WASM file
+WASM_FILE='public/js/hyphenopoly/patterns/en-gb.wasm'
+gzip -k $WASM_FILE
+mv "${WASM_FILE}.gz" $WASM_FILE
+
+# Rename .html files (except index.html and 404.html)
+find public/ -type f ! -iname 'index.html' ! -iname '404.html' -iname '*.html' -print0 | while read -d $'\0' f; do
+  mv "$f" "${f%.html}"
+done
+
+# Sync files to S3
+aws s3 sync ./public/ s3://$S3_BUCKET/ --delete --cache-control "max-age=31556952" --exclude "*" --include "js/hyphenopoly/patterns/en-gb.wasm" --content-encoding gzip --content-type application/wasm
+aws s3 sync ./public/ s3://$S3_BUCKET/ --delete --cache-control "max-age=31556952" --exclude "*" --include "css/*" --include "js/*" --exclude js/hyphenopoly/patterns/en-gb.wasm --include "fonts/*"
+aws s3 sync ./public/ s3://$S3_BUCKET/ --delete --exclude "css/*" --exclude "js/*" --exclude "fonts/*"
+
+# Create CloudFront invalidation
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --paths "/*"
